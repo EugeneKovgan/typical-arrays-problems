e2f17e8c9f4bb4340fcf4825697109548f3679a1
EugeneKovgan <56769548+EugeneKovgan@users.noreply.github.com>
2020-03-23 23:42:25 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (array == undefined || array.length == 0) {
+        return 0 ;
+    } else {
+    let min = 0; 
+    min = Math.min.apply(null, array);   
+    return min;
+}}
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (array == undefined || array.length == 0) {
+        return 0 ;
+    } else {
+    let max = 0; 
+    max = Math.max.apply(null, array);
+    return max;
+}}
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg (array) {  
+    if (array == undefined || array.length == 0) {
+        return 0 ;
+    } else {
+  let avg=0;
+  let avg1=0;
+  for (i=0; i < array.length; i++ ) {
+    avg1 +=array[i];
+    avg = avg1 / array.length;
+
+  }
+    return avg;
+}}
